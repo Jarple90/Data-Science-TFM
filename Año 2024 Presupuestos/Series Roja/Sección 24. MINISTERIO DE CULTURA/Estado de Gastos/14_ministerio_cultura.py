3bb4a9ae2b285e89c96ad91f5cc63e6810573749
José Antonio Romero Pérez <jarptgd@gmail.com>
2025-09-03 20:15:15 +0200

    commit

---
+++ Año 2024 Presupuestos/Series Roja/Sección 24. MINISTERIO DE CULTURA/Estado de Gastos/14_ministerio_cultura.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+ruta = r"R:\Data Science TFM\Año 2024 Presupuestos\Series Roja\Sección 24. MINISTERIO DE CULTURA\Estado de Gastos\14_ministerio_cultura.CSV"
+# Leer el CSV con punto y coma como separador
+df = pd.read_csv(ruta, encoding="latin1", sep=";")
+
+# Mostrar las primeras filas
+print(df.head())
---
+++ Año 2024 Presupuestos/Series Roja/Sección 25. MINISTERIO DE VIVIENDA Y AGENDA URBANA/Estado de Gastos/13_ministerio_vivienda.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+ruta = r"R:\Data Science TFM\Año 2024 Presupuestos\Series Roja\Sección 25. MINISTERIO DE VIVIENDA Y AGENDA URBANA\Estado de Gastos\13_ministerio_vivienda.CSV"
+# Leer el CSV con punto y coma como separador
+df = pd.read_csv(ruta, encoding="latin1", sep=";")
+
+# Mostrar las primeras filas
+print(df.head())
