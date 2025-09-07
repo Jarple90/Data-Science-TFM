062dd79c510ee8459e8a1841619db11bbc6ccbd9
José Antonio Romero Pérez <jarptgd@gmail.com>
2025-09-07 23:10:57 +0200

    progreso y modificaciones

---
+++ Año 2024 Presupuestos/Serie Verde/ESTADO/Estructura económica de los recursos asignados a los centros gestores para cada programa de gasto/15. MINISTERIO DE HACIENDA/02_ministerio_hacienda.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+ruta = r"R:\Data Science TFM\Año 2024 Presupuestos\Serie Verde\ESTADO\Estructura económica de los recursos asignados a los centros gestores para cada programa de gasto\15. MINISTERIO DE HACIENDA\02_ministerio_hacienda.CSV"
+# Leer el CSV con punto y coma como separador
+df = pd.read_csv(ruta, encoding="latin1", sep=";")
+
+# Reemplazar los NaN
+df.fillna('', inplace=True)  # Los NaN se convierten en celdas vacías
+
+# Mostrar las primeras filas
+print(df.head())
