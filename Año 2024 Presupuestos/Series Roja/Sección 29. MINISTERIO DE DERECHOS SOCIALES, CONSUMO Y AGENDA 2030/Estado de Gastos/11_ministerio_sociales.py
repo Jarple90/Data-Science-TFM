5b7e4f1bc10857a6ccf168cece38aa591ddb06e7
José Antonio Romero Pérez <jarptgd@gmail.com>
2025-09-08 12:46:01 +0200

    modificaciones y mejoras

--- Año 2024 Presupuestos/Series Roja/Sección 29. MINISTERIO DE DERECHOS SOCIALES, CONSUMO Y AGENDA 2030/Estado de Gastos/11_ministerio_sociales.py
+++ Año 2024 Presupuestos/Series Roja/Sección 29. MINISTERIO DE DERECHOS SOCIALES, CONSUMO Y AGENDA 2030/Estado de Gastos/11_ministerio_sociales.py
@@ -4,5 +4,8 @@ ruta = r"R:\Data Science TFM\Año 2024 Presupuestos\Series Roja\Sección 29. MIN
 # Leer el CSV con punto y coma como separador
 df = pd.read_csv(ruta, encoding="latin1", sep=";")
 
+# Reemplazar los NaN
+df.fillna('', inplace=True)  # Los NaN se convierten en celdas vacías
+
 # Mostrar las primeras filas
 print(df.head())
