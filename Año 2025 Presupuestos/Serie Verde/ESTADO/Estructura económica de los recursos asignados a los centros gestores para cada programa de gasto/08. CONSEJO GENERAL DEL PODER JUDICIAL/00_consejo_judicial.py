d031b8bbbcd195886380aec1c8f2c8432e6ac02d
José Antonio Romero Pérez <jarptgd@gmail.com>
2025-09-13 20:05:27 +0200

    modificaciones e implementaciones

---
+++ Año 2025 Presupuestos/Serie Verde/ESTADO/Estructura económica de los recursos asignados a los centros gestores para cada programa de gasto/08. CONSEJO GENERAL DEL PODER JUDICIAL/00_consejo_judicial.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+ruta = r"R:\Data Science TFM\Año 2024 Presupuestos\Serie Verde\ESTADO\Estructura económica de los recursos asignados a los centros gestores para cada programa de gasto\08. CONSEJO GENERAL DEL PODER JUDICIAL\00_consejo_judicial.CSV"
+# Leer el CSV con punto y coma como separador
+df = pd.read_csv(ruta, encoding="latin1", sep=";")
+
+# Reemplazar los NaN
+df.fillna('', inplace=True)  # Los NaN se convierten en celdas vacías
+
+# Mostrar las primeras filas
+print(df.head())
